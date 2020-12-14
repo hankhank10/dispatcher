f7532c70b5ea4013a420fffd4b194a2961631cd4
hankhank10 <m.a.hankinson@gmail.com>
2020-12-14 10:15:53 +0000

    Update dispatcherCustomJS.js

--- project/static/js/dispatcherCustomJS.js
+++ project/static/js/dispatcherCustomJS.js
@@ -1,24 +1,27 @@
 // Creates a red marker with the coffee icon
-let markerGroup;
+let calloutMarkerGroup;
+let responseUnitMarkerGroup;
 
 function testAlert() {
     alert ("Hello!")
 }
 
 function initialiseMap() {
-    markerGroup = L.layerGroup().addTo(map);
+    calloutMarkerGroup = L.layerGroup().addTo(map);
+    responseUnitMarkerGroup = L.layerGroup().addTo(map);
 
 }
 
 function addMarker(markerType, colour, latitude, longitude) {
 
-    if (markerType == 'incident') {
+    if (markerType == 'callout') {
         var iconMarker = L.ExtraMarkers.icon({
             icon: 'fa-bell',
             markerColor: colour,
             shape: 'star',
             prefix: 'fa'
         });
+        var new_marker = L.marker([latitude, longitude], {icon: iconMarker}).addTo(calloutMarkerGroup);
     }
 
     if (markerType == 'response_unit') {
@@ -28,34 +31,45 @@ function addMarker(markerType, colour, latitude, longitude) {
             shape: 'star',
             prefix: 'fas'
         });
+        var new_marker = L.marker([latitude, longitude], {icon: iconMarker}).addTo(responseUnitMarkerGroup);
     }
+}
+
+
+function clearMarkers(markerType) {
     
-    var new_marker = L.marker([latitude, longitude], {icon: iconMarker}).addTo(markerGroup);
+    if (markerType == 'callout') {
+        map.removeLayer(calloutMarkerGroup);
+        calloutMarkerGroup = L.layerGroup().addTo(map);
+    }
+
+    if (markerType == 'response_unit') {
+        map.removeLayer(responseUnitMarkerGroup);
+        responseUnitMarkerGroup = L.layerGroup().addTo(map);
+    }
+
+    if (markerType == 'all') {
+        clearMarkers('callout');
+        clearMarkers('response_unit');
+    }
+
 }
 
-function clearMarkers() {
-    map.removeLayer(markerGroup);
-    markerGroup = L.layerGroup().addTo(map);
-
-}
 
 function updateCallouts(){
     
-    // Clear previous map
-    clearMarkers()
+    // Clear previous map layer
+    clearMarkers('callout');
     
     // Load new points
     $.getJSON($SCRIPT_ROOT + '/json/callout_data', {}, function(data) {
-
         $.each(data, function(n, element) {
-            addMarker('incident', 'red', element.latitude, element.longitude)
+            addMarker('callout', 'red', element.latitude, element.longitude)
         });
-
     })
 }
 
+
 function pingURL(urlToPing){
-
     $.get($SCRIPT_ROOT + urlToPing, function(data) {});
-
 }
