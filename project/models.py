2dda786ac2436457b43f917c55d85185191c6da3
hankhank10 <m.a.hankinson@gmail.com>
2020-12-15 15:09:11 +0000

    Update

--- project/models.py
+++ project/models.py
@@ -12,11 +12,26 @@ class Callout(db.Model):
 
 class ResponseUnit(db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+
+    # Where the unit is
     last_known_latitude = db.Column(db.Float)
     last_known_longitude = db.Column(db.Float)
+    
+    # What the unit is called
+    shift_name = db.Column(db.String)
+    unit_name = db.Column(db.String)
+    
+    @property
+    def callsign (self):
+        return self.shift_name + "/" + self.unit_name
+
+
+    # What the unit is doing
     status = db.Column(db.String)   # hidden, available, en route, on scene, inactive
     current_path = db.Column(db.Integer)
-
+    responding_to_callout = db.Column(db.Integer)
+    
+    
 
 class Path(db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
